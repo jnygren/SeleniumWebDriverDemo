36a726ba909290f52d92e0deef9e58a7ee6dfeeb
Jeff Nygren <JNygren.Programmer@GMail.com>
2018-03-14 13:04:14 -0500

    Added NLog logging to project.

--- SeleniumWebDriverDemo/MainWindow.xaml.cs
+++ SeleniumWebDriverDemo/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿// App.xaml.cs
 
 using System.Windows;
+using NLog;
 
 namespace SeleniumWebDriverDemo
 {
@@ -9,12 +10,37 @@ namespace SeleniumWebDriverDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Logger logger = LogManager.GetCurrentClassLogger();
+
         /// <summary>
         /// c'tor
         /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Window Loaded event handler
+        /// </summary>
+        /// <remarks>
+        ///    To add handlers like this, 
+        ///    1. open .xaml and select <window> element. 
+        ///    2. In Properties panel at right, click 'events', then double-click the event you want a handler for.
+        /// </remarks>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            logger.Debug("---  Program started.  ---");
+        }
+
+
+        /// <summary>
+        /// Window Closed event handler
+        /// </summary>
+        private void Window_Closed(object sender, System.EventArgs e)
+        {
+            logger.Debug("---  End of Program.  ---\n");
         }
 
 
@@ -28,10 +54,11 @@ namespace SeleniumWebDriverDemo
 
 
         /// <summary>
-        /// Close program ('Exit' menu item event handler)
+        /// Close program ('About' menu item event handler)
         /// </summary>
         private void HelpAbout_Click(object sender, RoutedEventArgs e)
         {
+            logger.Info("About box displayed.");
             About about = new About();
             about.Owner = this;
             about.ShowInTaskbar = false;
